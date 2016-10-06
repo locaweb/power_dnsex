0e11298372ccdc8dbaaf47516d4372d5a105886e
Rodrigo Coutinho <rodrigo.coutinho@locaweb.com.br>
2016-10-06 16:15:18 -0300

    Package information

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PowerDNSex.Mixfile do
 
   def project do
     [app: :powerdnsex,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -18,5 +18,18 @@ defmodule PowerDNSex.Mixfile do
     [{:httpoison, "~> 0.9.0"},
      {:poison, "~> 2.2"},
      {:exvcr, "~> 0.8.0", only: :test}]
+  end
+
+  defp description do
+    """
+    A Client to integrate with PowerDNS API version 4
+    """
+  end
+
+  defp package do
+    [ maintainers: ["Rodrigo Coutinho"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/digaoddc/power_dnsex"}
+    ]
   end
 end
