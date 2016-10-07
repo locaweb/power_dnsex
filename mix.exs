6fa78ab4c8e6124297127f48f4766e206662d675
Rodrigo Coutinho <rodrigo.coutinho@locaweb.com.br>
2016-10-07 11:27:05 -0300

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PowerDNSex.Mixfile do
 
   def project do
     [app: :powerdnsex,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
