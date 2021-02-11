03a28c7dd455f3e0d1d37f4c4bc8c4634ba4505c
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2021-02-11 14:44:08 -0300

    Bump to 0.5.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PowerDNSex.Mixfile do
   def project do
     [
       app: :powerdnsex,
-      version: "0.4.1",
+      version: "0.5.0",
       elixir: "~> 1.6",
       description: description(),
       package: package(),
